722d45446551a662548e35ad190cdedc9dbcb4b0
DrMartiner <DrMartiner@GMail.Com>
2012-03-25 12:32:04 +0700

    При удаление пользователя будут удаляться PublicID, Profile и RegistrationProfile

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from social_auth.signals import socialauth_registered
-from social_auth.models import UserSocialAuth
+from django.db.models.signals import post_delete
 
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from publicauth.models import PublicID
+from registration.models import RegistrationProfile
 
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    phone = models.CharField('Телефон', help_text='Контактный телефон автора работ', max_length=32, null=True, blank=True)
     did_training = models.BooleanField('Прошел разминку', default=False)
     scores = models.FloatField('Кол-во правильных ответов', default=0.0)
 
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-post_save.connect(create_user_profile, sender=User)
+def delete_user(instance, **kwargs):
+    Profile.objects.filter(user=instance).delete()
+    PublicID.objects.filter(user=instance).delete()
+    RegistrationProfile.objects.filter(user=instance).delete()
+    instance.delete()
+post_delete.connect(delete_user, sender=User)
