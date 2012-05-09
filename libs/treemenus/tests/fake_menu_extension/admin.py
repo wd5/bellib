63755ce7f38850dfbf8fa621a89ac0b06fcb25e0
DrMartiner <DrMartiner@GMail.Com>
2012-05-09 17:42:45 +0400

    Грохнул библиотеки (перезжаем на virtual env)

--- libs/treemenus/tests/fake_menu_extension/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-        
-from utils.treemenus.tests.fake_menu_extension.models import FakeMenuItemExtension
-        
-admin.site.register(FakeMenuItemExtension)
--- libs/treemenus/tests/fake_menu_extension/models.py
+++
@@ -1,7 +0,0 @@
-
-from django.db import models
-from treemenus.models import MenuItem
-
-class FakeMenuItemExtension(models.Model):
-    menu_item = models.OneToOneField (MenuItem, related_name="%(class)s_related")
-    published = models.BooleanField(default=False)
